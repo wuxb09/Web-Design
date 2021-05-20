762b6253a9bf8e36605173599e682e3389b386e2
Xiaoban Wu <xiaoban_wu@student.uml.edu>
2021-05-19 23:58:06 -0700

    update react aboutus with leaders

--- react/src/components/MainComponent.js
+++ react/src/components/MainComponent.js
@@ -5,6 +5,7 @@ import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import About from './AboutComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import { COMMENTS } from '../shared/comments';
@@ -45,6 +46,7 @@ class Main extends Component {
         <Header />
         <Switch>
               <Route path='/home' component={HomePage} />
+              <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders}/>} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route exact path='/contactus' component={Contact} />
               <Route path='/menu/:dishId' component={DishWithId} />
