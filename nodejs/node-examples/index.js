62b43e9f9343021fbc58fcad60c00800653a4485
Xiaoban Wu <xiaoban_wu@student.uml.edu>
2021-05-20 22:33:19 -0700

    update nodejs examples with callback scope and require cache

--- nodejs/node-examples/index.js
+++ nodejs/node-examples/index.js
@@ -1,20 +1,40 @@
 var rect = require('./rectangle');
+var tmp = require('./test_require.js'); //test if require has cache effect
+
+function callback(err, rectangle) {
+    if (err) {
+        console.log("ERROR: ", err.message);
+    }
+    else {
+        console.log("Testing callback scope " + xxx + "!!!!!!!!"); //scope error!
+        console.log("The area of the rectangle of dimensions l = "
+            + l + " and b = " + b + " is " + rectangle.area());
+        console.log("The perimeter of the rectangle of dimensions l = "
+            + l + " and b = " + b + " is " + rectangle.perimeter());
+    }
+}
 
 function solveRect(l,b) {
     console.log("Solving for rectangle with l = "
                 + l + " and b = " + b);
-    rect(l,b, (err,rectangle) => {
+    var xxx = "hello";
+    //js callback look more likes a jump point. 
+    //When the subroutine reaches the callback, it simply jumps back to this point with new parameter and continue executing
+    //once over, and back to the subroutine and continue there
+    rect.calc(l,b, (err,rectangle) => {
         if (err) {
 	        console.log("ERROR: ", err.message);
 	    }
         else {
+            console.log("Testing callback scope " + xxx + "!!!!!!!!");//scope success!
             console.log("The area of the rectangle of dimensions l = "
                 + l + " and b = " + b + " is " + rectangle.area());
             console.log("The perimeter of the rectangle of dimensions l = "
                 + l + " and b = " + b + " is " + rectangle.perimeter());
         }
     });
-    console.log("This statement after the call to rect()");
+    //rect.calc(l,b, callback);
+    console.log("This statement after the call to rect() " + rect.message);
 };
 
 solveRect(2,4);
